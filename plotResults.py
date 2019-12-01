567a078340fa17b81a2444b26dabc47fac03d7cb
Ken Smith <k.w.smith@qub.ac.uk>
2019-12-01 22:15:39 +0000

    Modified plotResults to call Darryl's version of plot_roc so that I can use the numbers returned to show a dotted line at the chosen missed detection rate.

--- plotResults.py
+++ plotResults.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 Usage:
-  %s <csvfile>... [--outputFile=<outputFile>]
+  %s <csvfile>... [--outputFile=<outputFile>] [--xlim=<xlimit>] [--ylim=<ylimit>]
   %s (-h | --help)
   %s --version
 
@@ -9,6 +9,8 @@ Options:
   -h --help                    Show this screen.
   --version                    Show version.
   --outputFile=<outputFile>    Default place to store the outputfile. [default: /tmp/output.png]
+  --xlim=<xlimit>              Plot x limit [default: 1.0]
+  --ylim=<ylimit>              Set y limit [default: 1.0]
 
 Example:
   %s output_results.csv
@@ -19,7 +21,9 @@ from docopt import docopt
 from gkutils import Struct, cleanOptions
 import numpy as np
 import pandas as pd
-from sklearn.metrics import roc_curve, auc
+#from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import auc
+from plotROC import roc_curve
 import optparse
 import matplotlib.pyplot as plt
 
@@ -31,28 +35,37 @@ def plot_roc(fpr, tpr,roc_auc,roc):
 	roc.set_title('ROC curve')
 	roc.legend(loc="lower right")
 
-def plot_tradeoff(mdr, fpr, tradeoff, intercept=[]):
+def plot_tradeoff(mdr, fpr, tradeoff, intercept=[], xlim = 1.0, ylim = 1.0, title = 'Detection Error Tradeoff'):
 	tradeoff.plot(mdr,fpr,lw=2)
 	if len(intercept) > 0:
 		tradeoff.plot([0,intercept[0],intercept[0],intercept[0]], [intercept[1],intercept[1],intercept[1],0], color='black', linestyle='--')
 	tradeoff.set_xlabel('Missed detection rate')
 	tradeoff.set_ylabel('False positive rate')
-	tradeoff.set_title('Detection Error Tradeoff')
-	tradeoff.set_xlim(0,0.25)
-	tradeoff.set_ylim(0,0.2)
+	tradeoff.set_title(title)
+	#tradeoff.set_xlim(0,0.25)
+	#tradeoff.set_ylim(0,0.2)
+	tradeoff.set_xlim(0,xlim)
+	tradeoff.set_ylim(0,ylim)
 
 
-def plotResults(files, outputfile):
+def plotResults(files, outputfile, options = None):
 	#fig, (roc, tradeoff) = plt.subplots(1,2,sharey=False)
 	fig, (tradeoff) = plt.subplots(1,1,sharey=False)
 	for file in files:
 		data = pd.read_csv(file, names=['file', 'tag', 'prediction'])
 		y = data['tag']
 		scores = data['prediction']
-		fpr,tpr,thresholds = roc_curve(y, scores, pos_label=1)
+		#fpr,tpr,thresholds = roc_curve(y, scores, pos_label=1)
+		fpr,tpr,thresholds = roc_curve(np.array(y), np.array(scores))
+
+		mdrSet = 0.04
+		fpr_at_mdrSet = (fpr[np.where(1-tpr<=mdrSet)[0]][-1])
+
+		print("[+]%.3lf%% mdr gives " % (mdrSet*100) + str(fpr[np.where(1-tpr<=mdrSet)[0]][-1]*100) + "% fpr")
+		print("   [+] threshold : %.3lf"%(thresholds[np.where(1-tpr<=mdrSet)[0]][-1]))
 		roc_auc = auc(fpr, tpr)	
 		mdr = 1-tpr
-		plot_tradeoff(mdr, fpr, tradeoff, intercept=[0.01,0.15499673988146792])
+		plot_tradeoff(mdr, fpr, tradeoff, intercept=[mdrSet,fpr_at_mdrSet], xlim = float(options.xlim), ylim = float(options.ylim), title = 'Detection Error Tradeoff (MLO)')
 		#plot_roc(fpr,tpr,roc_auc,roc)
 #	plt.savefig(outputfile)
 	plt.show()
@@ -65,7 +78,7 @@ def main():
     # Use utils.Struct to convert the dict into an object for compatibility with old optparse code.
     options = Struct(**opts)
     print (options.csvfile)
-    plotResults(options.csvfile, options.outputFile)
+    plotResults(options.csvfile, options.outputFile, options = options)
 
 
 if __name__=='__main__':
